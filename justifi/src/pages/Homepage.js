654c59096889d4beab1fd8117bd0f0296e42a928
AzmathKhan <azmathayan21@gmail.com>
2025-03-21 01:13:38 -0600

    Add Justifi AI project files properly

---
+++ justifi/src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import About from "../components/About";
+
+function Homepage() {
+  return (
+    <div>
+      <Header />
+      {/* <About /> */}
+      {/* <Features/> */}
+      {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default Homepage;
